bdc7dc1ac84a61287902088d567a4944e1f0ac0d
karolwen <karoline.wennberg@gmail.com>
2020-03-24 10:15:26 +0100

    CPU & GPU Classes
    CPU & GPU classes

---
+++ src/main/java/org/oslomet/ComponentClasses/CPUModel.java
@@ -0,0 +1,32 @@
+package org.oslomet.ComponentClasses;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class CPUModel extends ComponentModel {
+
+    SimpleIntegerProperty intClockSpeed, intCores;
+    SimpleDoubleProperty doubleFrequency;
+
+    public CPUModel(String txtName, String txtBrand, double doublePrice, double doublePerformanceValue, int intClockSpeed, double doubleFrequency, int intCores) {
+        super(txtName, txtBrand, doublePrice, doublePerformanceValue);
+        this.intClockSpeed = new SimpleIntegerProperty(intClockSpeed);
+        this.doubleFrequency = new SimpleDoubleProperty(doubleFrequency);
+        this.intCores = new SimpleIntegerProperty(intCores);
+
+    }
+
+    public int getIntClockSpeed() {return intClockSpeed.get(); }
+
+    public void setIntClockSpeed(int intClockSpeed) {this.intClockSpeed.set(intClockSpeed); }
+
+    public double getDoubleFrequency() {return doubleFrequency.get(); }
+
+    public void setDoubleFrequency(int doubleFrequency) {this.doubleFrequency.set(doubleFrequency); }
+
+    public int getIntCores() {return intCores.get(); }
+
+    public void setIntCores(int intCores) {this.intCores.set(intCores); }
+
+}
+
---
+++ src/main/java/org/oslomet/ComponentClasses/GPUModel.java
@@ -0,0 +1,24 @@
+package org.oslomet.ComponentClasses;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class GPUModel extends ComponentModel {
+
+    SimpleIntegerProperty intClockSpeed, intMemory;
+
+    public GPUModel(String txtName, String txtBrand, double doublePrice, double doublePerformanceValue, int intClockSpeed, int intMemory) {
+        super(txtName, txtBrand, doublePrice, doublePerformanceValue);
+        this.intClockSpeed = new SimpleIntegerProperty(intClockSpeed);
+        this.intMemory = new SimpleIntegerProperty(intMemory);
+
+    }
+
+        public int getIntClockSpeed() {return intClockSpeed.get(); }
+
+        public void setIntClockSpeed(int intClockSpeed) {this.intClockSpeed.set(intClockSpeed); }
+
+        public int getIntMemory() {return intMemory.get(); }
+
+        public void setIntMemory(int intMemory) {this.intMemory.set(intMemory); }
+    }
+
