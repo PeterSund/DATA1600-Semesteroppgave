c1549e4944d2f8edb6c262c82adc40f6d756ee2d
karolwen <karoline.wennberg@gmail.com>
2020-03-30 13:25:47 +0200

    Admin controller

--- src/main/java/org/oslomet/AdminController.java
+++ src/main/java/org/oslomet/AdminController.java
@@ -1,4 +1,84 @@
 package org.oslomet;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class AdminController {
+
+    @FXML
+    private Button btnLogOut;
+
+    @FXML
+    private TableView<?> tvComputercase;
+
+    @FXML
+    private Button btnCPU;
+
+    @FXML
+    private Button btnGPU;
+
+    @FXML
+    private Button btnHarddrive;
+
+    @FXML
+    private Button btnMotherboard;
+
+    @FXML
+    private Button btnRAM;
+
+    @FXML
+    private Button btnSoundcard;
+
+    @FXML
+    private Button btnPSU;
+
+    @FXML
+    private Button btnMonitor;
+
+    @FXML
+    private Button btnMouse;
+
+    @FXML
+    private Button btnComputercase;
+
+    @FXML
+    private Button btnKeyboard;
+
+    @FXML
+    private Button btnAddComponent;
+
+    @FXML
+    private Button btnLoadComponent;
+
+    @FXML
+    private Button btnSaveComponent;
+
+    @FXML
+    private TableView<?> tvCPU;
+
+    @FXML
+    private TableView<?> tvHarddrive;
+
+    @FXML
+    private TableView<?> tvMotherboard;
+
+    @FXML
+    private TableView<?> tvRAM;
+
+    @FXML
+    private TableView<?> tvSoundcard;
+
+    @FXML
+    private TableView<?> tvPSU;
+
+    @FXML
+    private TableView<?> tvMonitor;
+
+    @FXML
+    private TableView<?> tvMouse;
+
+    @FXML
+    private TableView<?> tvKeyboard;
+
 }
