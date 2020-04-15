da0174326ab076d25e45f69632e1670c1244ff9b
zaktaw <zaktaw@gmail.com>
2020-04-15 09:25:00 +0200

    Removed comments

--- src/main/java/org/oslomet/ComponentRegistry/ComponentsRegistry.java
+++ src/main/java/org/oslomet/ComponentRegistry/ComponentsRegistry.java
@@ -12,12 +12,12 @@ public class ComponentsRegistry {
     private static ArrayList<ArrayList> allComponents = new ArrayList<>();
 
     public static ArrayList addAllComponentsArraysToArray() {
-        //allComponents.add(ComputerCaseRegistry.returnArray());
+        allComponents.add(ComputerCaseRegistry.returnArray());
         allComponents.add(CPURegistry.returnArray());
         allComponents.add(GPURegistry.returnArray());
-        //allComponents.add(HardDriveRegistry.returnArray());
-        //allComponents.add(KeyboardRegistry.returnArray());
-        //allComponents.add(MonitorRegistry.returnArray());
+        allComponents.add(HardDriveRegistry.returnArray());
+        allComponents.add(KeyboardRegistry.returnArray());
+        allComponents.add(MonitorRegistry.returnArray());
         allComponents.add(MotherboardRegistry.returnArray());
         allComponents.add(MouseRegistry.returnArray());
         allComponents.add(PSURegistry.returnArray());
